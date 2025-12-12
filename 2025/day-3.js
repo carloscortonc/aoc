e406f4a2b7b71225c913a107677c9efae77f0a20
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2025-12-12 10:33:04 +0100

    chore: unify into a single solver

--- 2025/day-3.js
+++ 2025/day-3.js
@@ -1,50 +1,25 @@
 // https://adventofcode.com/2025/day/3
 
-const part_1 = (i, debug) => {
+const solver = ({ i, nDigits, debug }) => {
   let count = 0;
   const logs = [];
   for (const bank of i) {
     logs.push(bank);
     const ratings = bank.split("");
-    let c = ratings.slice(0, 2);
+    let c = ratings.slice(0, nDigits);
     let prevC = ""; // for debugging purposes
+    let skipped = 0; // keep track of how many ratings we have skipped when generating a new candidate
     for (let i = 1; i < ratings.length; i++) {
       prevC.toString() !== c.toString() && logs.push("   [current candidate] ".concat(c.join("")));
       prevC = [...c];
-      // Early return if we already found the largest possible (99)
-      if (c.join("") == "99") break;
-      if (i < ratings.length - 1 && ratings[i] > c[0]) {
-        c = ratings.slice(i, i + 2);
-      } else if (ratings[i] > c[1]) {
-        c[1] = ratings[i];
-      }
-    }
-    count += parseInt(c.join(""));
-  }
-  debug && console.log(logs.join("\n"));
-  return count;
-};
-
-const part_2 = (i, debug) => {
-  let count = 0;
-  const logs = [];
-  const maxLength = 12;
-  for (const bank of i) {
-    logs.push(bank);
-    const ratings = bank.split("");
-    let c = ratings.slice(0, maxLength);
-    let prevC = ""; // for debugging purposes
-    let skipped = 0;
-    for (let i = 1; i < ratings.length; i++) {
-      prevC.toString() !== c.toString() && logs.push("   [current candidate] ".concat(c.join(""), ", i=", i));
-      prevC = [...c];
       const rating = ratings[i];
-      const startIndex = Math.max(maxLength - ratings.length + i, 0);
+      // The start index to search on depends on how many ratings are left after the last candidate digit
+      const startIndex = Math.max(nDigits - ratings.length + i, 0);
       // Early return if we already found the largest possible
-      if (c.join("") === "9".repeat(maxLength)) break;
-      for (let d = startIndex; d < maxLength && skipped + maxLength < ratings.length && skipped + d < i; d++) {
+      if (c.join("") === "9".repeat(nDigits)) break;
+      for (let d = startIndex; d < nDigits && skipped + nDigits < ratings.length && skipped + d < i; d++) {
         if (rating > c[d]) {
-          c = c.slice(0, d).concat(ratings.slice(i, i + maxLength - d));
+          c = c.slice(0, d).concat(ratings.slice(i, i + nDigits - d));
           skipped = i - d;
           break;
         }
@@ -55,6 +30,9 @@ const part_2 = (i, debug) => {
   debug && console.log(logs.join("\n"));
   return count;
 };
+
+const part_1 = (i) => solver({ i, nDigits: 2 });
+const part_2 = (i) => solver({ i, nDigits: 12 });
 
 const i = await fetch("https://adventofcode.com/2025/day/3/input")
   .then((r) => r.text())
