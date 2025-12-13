9814693ec13defd96c4a5df7d7c79ad8f24af18d
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2025-12-13 11:14:28 +0100

    feat: day-5 part-2

--- 2025/day-5.js
+++ 2025/day-5.js
@@ -18,8 +18,38 @@ let part_1 = (i) => {
   return count;
 };
 
+let part_2 = (i) => {
+  const sliceIndex = i.findIndex((e) => !e);
+  // Sort the list of ranges by start-value
+  const ranges = i.slice(0, sliceIndex).sort((a, b) => {
+    const p = [a, b].map((e) => e.split("-"));
+    const sdiff = p[0][0] - p[1][0];
+    return sdiff === 0 ? p[0][1] - p[1][1] : sdiff;
+  });
+  const parseRange = (r) => r.split("-").map((e) => parseInt(e));
+  for (let ri = 0; ri < ranges.length - 1; ri++) {
+    const [curr, next] = [ri, ri + 1].map((index) => parseRange(ranges[index]));
+    // Check to dismiss next range (contained inside first)
+    if (curr[1] >= next[1]) {
+      ranges.splice(ri + 1, 1);
+      ri--;
+    }
+    // Check to concatenate both ranges
+    else if (curr[1] >= next[0]) {
+      ranges[ri] = "".concat(curr[0], "-", next[1]);
+      ranges.splice(ri + 1, 1);
+      ri--;
+    }
+  }
+  return ranges.reduce((acc, curr) => {
+    const s = curr.split("-").map((e) => parseInt(e));
+    return acc + s[1] - s[0] + 1;
+  }, 0);
+};
+
 const i = await fetch("https://adventofcode.com/2025/day/5/input")
   .then((r) => r.text())
   .then((raw) => raw.trim().split("\n"));
 
 console.log("Part 1: ", part_1(i));
+console.log("Part 2: ", part_2(i));
