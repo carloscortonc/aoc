8707e7b9d4a1956c6a56f490dc3a46bf52883a7e
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2025-12-15 10:16:55 +0100

    feat: day-7 part-2

--- 2025/day-7.js
+++ 2025/day-7.js
@@ -1,26 +1,34 @@
 // https://adventofcode.com/2025/day/7
 
-let part_1 = (i) => {
+let solver = (i) => {
   let count = 0;
   const m = i.map((r) => r.split(""));
-  let beamPs = [m[0].findIndex((e) => e === "S")];
+  const startIndex = m[0].findIndex((e) => e === "S");
+  // Use object to track how many beans land on each position. Beam indexes are still `Object.keys(beamPsObj)`
+  let beamPsObj = { [startIndex]: 1 };
   for (let r = 0; r < m.length - 1; r++) {
-    let nextBeamPs = new Set();
+    let nextBeamPsObj = {};
+    const beamPs = Object.keys(beamPsObj).map((e) => parseInt(e));
     for (let bIndex = 0; bIndex < beamPs.length; bIndex++) {
       const c = beamPs[bIndex];
+      const add = (e) => (nextBeamPsObj[e] = (nextBeamPsObj[e] || 0) + beamPsObj[c]);
       if (m[r + 1][c] === ".") {
-        nextBeamPs.add(c);
+        add(c);
         continue;
       }
       // We encounter a splitter "^"
       count++;
-      if (c > 0) nextBeamPs.add(c - 1);
-      if (c < m.length - 1) nextBeamPs.add(c + 1);
+      if (c > 0) add(c - 1);
+      if (c < m[0].length - 1) add(c + 1);
     }
-    beamPs = [...nextBeamPs];
+    beamPsObj = { ...nextBeamPsObj };
   }
-  return count;
+  const timelines = Object.values(beamPsObj).reduce((acc, curr) => acc + curr, 0);
+  return { count, timelines };
 };
+
+let part_1 = (i) => solver(i).count;
+let part_2 = (i) => solver(i).timelines;
 
 execute(async () => {
   const i = await fetch("https://adventofcode.com/2025/day/7/input")
@@ -28,4 +36,5 @@ execute(async () => {
     .then((raw) => raw.trim().split("\n"));
 
   console.log("Part 1: ", part_1(i));
+  console.log("Part 2: ", part_2(i));
 });
